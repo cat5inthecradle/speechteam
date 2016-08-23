c50498a496811451ff0b4221dfc7b118d4a7a27e
Darin Webb <darinlwebb@gmail.com>
2016-08-22 19:46:31 -0500

    deleted all the things

--- database/factories/SchoolFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$factory->define(App\School::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->city . " High School",
-    ];
-});
--- database/factories/StudentFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$factory->define(App\Student::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->firstName . ' ' . $faker->lastName,
-    ];
-});
